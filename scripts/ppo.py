2ac7bcbace5b0aa5739ed248c0b181c1569c9486
Shreyas Raorane <raorane@seas.upenn.edu>
2026-03-12 00:32:10 -0400

    getting stuck in local minima, 2 motors max, 2 motors min, increasing entropy for more exploration

--- scripts/ppo.py
+++ scripts/ppo.py
@@ -1,6 +1,6 @@
 import torch
 
-def update(policy, optimizer, buffer, clip_epsilon=0.2, value_loss_coef=0.5, entropy_coef=0.01, n_epochs=5, n_minibatches=4):
+def update(policy, optimizer, buffer, clip_epsilon=0.2, value_loss_coef=0.5, entropy_coef=0.03, n_epochs=5, n_minibatches=4):
 
     obs, actions, old_log_probs, advantages, returns = buffer.get()
 
